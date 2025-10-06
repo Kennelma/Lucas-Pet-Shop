2d3748a21997ca9070c3628e382505a757c11b67
dayanlp <dayanalop0917@gmail.com>
2025-10-06 17:02:03 -0600

    rama actualizada y sin problemas

--- FRONTEND/src/routes.js
+++ FRONTEND/src/routes.js
@@ -14,12 +14,7 @@ const Promociones = React.lazy(() => import('./views/promociones/Promociones'))
 const routes = [
   { path: '/Dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/clientes', name: 'Clientes', element: Clientes },
-<<<<<<< HEAD
   { path: '/productos/alimentos', name: 'Productos / Alimentos', element: Alimentos },
-=======
-
-  { path: '/productos/Alimentos', name: 'Productos / Alimentos', element: Alimentos },
->>>>>>> a91a094d282b63354d24e04a2ff260449eaf1b27
   { path: '/productos/animales', name: 'Productos / Animales', element: Animales },
   { path: '/productos/accesorios', name: 'Productos / Accesorios', element: Accesorios },
   { path: '/productos/medicamentos', name: 'Productos / Medicamentos', element: Medicamentos },
