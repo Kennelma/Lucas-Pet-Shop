f4b4bcf83dcbc7aaa7c8db6ccc8e447c5b8375e0
Kennelma <kennelmahernandez@gmail.com>
2025-10-05 22:57:21 -0600

    Antes de nuevos cambios

--- FRONTEND/src/routes.js
+++ FRONTEND/src/routes.js
@@ -5,7 +5,7 @@ const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 const Clientes = React.lazy(() => import('./views/clientes/Clientes'))
 
 const Accesorios = React.lazy(() => import('./views/productos/accesorios/Accesorios'))
-const Aliumentos = React.lazy(() => import('./views/productos/alimentos/Alimentos'))
+const Alimentos = React.lazy(() => import('./views/productos/alimentos/Alimentos'))
 const Medicamentos = React.lazy(() => import('./views/productos/medicamentos'))
 const Animales = React.lazy(() => import('./views/productos/animales/Animales'))
 
@@ -19,13 +19,13 @@ const routes = [
   { path: '/Dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/clientes', name: 'Clientes', element: Clientes },
 
-  { path: '/productos/alimentos', name: 'Productos / Alimentos', element: Aliumentos },
+  { path: '/productos/alimentos', name: 'Productos / Alimentos', element: Alimentos },
   { path: '/productos/animales', name: 'Productos / Animales', element: Animales },
   { path: '/productos/medicamentos', name: 'Productos / Medicamentos', element: Medicamentos },
   { path: '/productos/accesorios', name: 'Productos / Accesorios', element: Accesorios },
 
-  { path: '/peluqueria-canina/Promociones', name: 'Peluqueria Canina / Promociones', element: Promociones },
-  { path: '/peluqueria-canina/Servicios-peluqueria', name: 'Peluqueria Canina / Servicios peluqueria', element: Servicios },
+  { path: '/peluqueria_canina/promociones', name: 'Peluqueria Canina / Promociones', element: Promociones },
+  { path: '/peluqueria_canina/servicios', name: 'Peluqueria Canina / Servicios peluqueria', element: Servicios },
 ]
 
 
