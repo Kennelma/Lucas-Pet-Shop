d7b1dfb80a7b2a2fb8dcaf374e168283fad632b8
Sara <tuemail@ejemplo.com>
2025-10-06 00:05:24 -0600

    Bajar cambios

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,8 +1,8 @@
-// Importaciones al inicio
+//Constante para el paquete Express
 const express = require('express');
 
-// Constantes
-const app = express();
+//constante para los metodos de express.
+var  app = express();
 
 //constante para el paquete de bodyparser.
 const bp = require('body-parser');
@@ -11,18 +11,17 @@ const bp = require('body-parser');
 const mysqlConnection = require('./config/conexion'); 
 
 //Enviando los datos de data-form a NODEJS API
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+const cors = require("cors");
 
 //Permitir todas las peticiones desde cualquier origen
-//app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
 
 //IMPORTACION DE LAS RUTAS
 app.use('/api', require('./routes/rutas'));
 
-// Iniciar servidor
 const PORT = 4000;
 app.listen(PORT, function() {
     console.log('🚀 Servidor en puerto ' + PORT);
