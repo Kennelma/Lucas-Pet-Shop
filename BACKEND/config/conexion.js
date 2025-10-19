4dac2ac37d738ea86c194b4ab8b35308f7b8bbb7
Kennelma <kennelmahernandez@gmail.com>
2025-10-18 18:00:47 -0600

    Rama actualizada

---
+++ BACKEND/config/conexion.js
@@ -0,0 +1,37 @@
+//Constante para el maquete MySQL
+const path = require('path');
+
+require('dotenv').config({
+    path: path.resolve(process.cwd(), '..', '.env.example') 
+});
+
+const mysql = require('mysql2/promise');
+console.log("Host de DB cargado:", process.env.DB_HOST);
+
+//CONFIGURACIÓN DE LA CONEXIÓN AL SERVIDOR CON LA BASE DE DATOS
+const mysqlConnection = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    multipleStatements: true,
+    ssl: {
+        rejectUnauthorized: false 
+    }
+});
+
+//MENSAJE DE CONFIRMACIÓN DE CONEXIÓN Y MANEJO DE ERRORES
+(async () => {
+    try {
+        await mysqlConnection.getConnection(); 
+        console.log('Conexión a MySQL OK');
+    } catch (err) {
+        console.log('Error conectando a MySQL:', err.message);
+        process.exit(1);
+    }
+})();
+
+
+//SE EXPORTA LA CONEXIÓN A LOS DIFERENTES ARCHIVOS
+module.exports = mysqlConnection;
