61f6fca15e5ed841bfb4eb9d4b8764439ebe52c7
Kennelma <kennelmahernandez@gmail.com>
2025-10-29 23:58:05 -0600

    Nuevos cambios entrantes

--- FRONTEND/src/views/facturacion/facturas/Facturacion.js
+++ FRONTEND/src/views/facturacion/facturas/Facturacion.js
@@ -11,7 +11,7 @@ export default function Facturacion () {
     const [activeTab, setActiveTab] = useState('nueva');
 
     return (
-        <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
+        <div className="min-h-screen bg-linear-to-br from-blue-50 to-indigo-100">
 
             {/* HEADER Y TÍTULO */}
             <div className="bg-white shadow-sm border-b border-gray-200">
