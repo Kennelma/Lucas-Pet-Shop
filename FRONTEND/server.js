84522743a0604676a48d1de17f9e1a143c862fdc
Kennelma <kennelmahernandez@gmail.com>
2025-11-22 00:37:48 -0600

    Prueba de algunos cambios de archivos, fron falta por desplegar

--- FRONTEND/server.js
+++ FRONTEND/server.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const distPath = path.join(__dirname, "dist");
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(distPath));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(distPath, "index.html"));
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`✅ Frontend server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log("Frontend running on port " + PORT);
 });
